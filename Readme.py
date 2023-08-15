536ad436486336d8a60ce3c76e96ce9db4df6967
nofreewill42 <jonavan01@gmail.com>
2023-08-15 17:25:07 +0200

    The mission

---
+++ Readme.py
@@ -0,0 +1,15 @@
+'''
+Make an open source pigbank logic SW, and replacable HW for it.
+It can count the money, and show the amount of money in various ways of HW implementations.
+It can show the amount/number of pieces of each type of coin,
+and separate them if wanted (this can also be done in a way that it can be added later as a HW module if you whished for it).
+
+Make a very basic version of this, that can also be sold as a DIY kit.
+If someone wanted, there can be even tutorials about how to make it, explaining how to use i.e. copilot to program the microcontroller,
+and/or sold also without the HW kit, making it a tutorial also about how to use gen AI to build these stuff from zero, selecting HW parts and all.
+
+What if almost anyone could just hack stuff together, and make it work?
+What and how many things could be built by any of us, for all of us?
+There are so many great minds out there, and so many great ideas (maybe not the pigbank lol), but not enough time to build them.
+That's what current AI models can help you out with.(/, and also make you familiarize yourself with it during the process, so you can even have a much better idea about where we stand with this thech right now.)
+'''
